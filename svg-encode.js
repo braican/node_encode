8ff99bd6743574102b5abb6055d310c11ef4b2ee
braican <nick.braica@gmail.com>
2015-11-20 10:43:17 -0500

    optimize the svg before copying it

--- svg-encode.js
+++ svg-encode.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var copypaste = require("copy-paste");
+var fs        = require("fs"),
+    copypaste = require("copy-paste"),
+    SVGO      = require("svgo"),
+    svgo      = new SVGO();
 
 if(!process.argv[2]){
     console.log("need to include a file");
@@ -11,9 +13,10 @@ fs.readFile(process.argv[2], 'utf8', function(error, data){
     if(error){
         return console.log(error);
     }
-    var encoded = encodeURIComponent( data );
-    copypaste.copy(encoded);
-    return console.log('copied');
-    //return console.log(encodeURIComponent( data ) );
-});
 
+    svgo.optimize(data, function(result) {
+        var encoded = encodeURIComponent( result.data );
+        copypaste.copy(encoded);
+        return console.log('copied');
+    }); 
+});
